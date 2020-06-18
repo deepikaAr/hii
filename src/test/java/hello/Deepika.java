161b23f55e42f9082759d97ba53d56b934b881b1
Deepika <deepikacoo@gmail.com>
2020-06-18 17:27:55 +0530

    bye

--- src/test/java/hello/Deepika.java
+++ src/test/java/hello/Deepika.java
@@ -4,6 +4,10 @@ public class Deepika {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("bye");
+		System.out.println("bye");
+		System.out.println("bye");
+		System.out.println("bye");
 
 	}
 
